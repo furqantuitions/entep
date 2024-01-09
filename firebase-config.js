427003077e11ef8472319a1183095e3c1c23f257
furqantuitions <150918578+furqantuitions@users.noreply.github.com>
2024-01-09 11:12:14 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+apiKey: "AIzaSyCbHb_MfnMuSJSLQsNc_y2Rh3olX8Qt_B8",
+  authDomain: "new-check-entep.firebaseapp.com",
+  projectId: "new-check-entep",
+  storageBucket: "new-check-entep.appspot.com",
+  messagingSenderId: "825195389121",
+  appId: "1:825195389121:web:15cccdb2421ff0e8057440",
+  measurementId: "G-KQLJBJM649"
+};
+
+firebase.initializeApp(firebaseConfig);
