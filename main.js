cf3b480b72a2d3ea38eee6577e769f2175ca62cf
furqantuitions <150918578+furqantuitions@users.noreply.github.com>
2024-01-10 11:09:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(user) {
+            alert("Login successful!");
+        })
+        .catch(function(error) {
+            alert("Error: " + error.message);
+        });
+}
+
+function register() {
+    var regEmail = document.getElementById("reg-email").value;
+    var regPassword = document.getElementById("reg-password").value;
+
+    firebase.auth().createUserWithEmailAndPassword(regEmail, regPassword)
+        .then(function(user) {
+            alert("Registration successful!");
+        })
+        .catch(function(error) {
+            alert("Error: " + error.message);
+        });
+}
+function toggleForm() {
+    var loginForm = document.getElementById('login-form');
+    var registerForm = document.createElement('div');
+    registerForm.innerHTML = `
+        <h2>Register</h2>
+        <form id="register-form">
+                    <label for="name"></label>
+            <input type="name" id="name" required placeholder="Enter your name" required>
+            <label for="reg-email"></label>
+            <input type="email" id="reg-email" placeholder="Enter your email" required>
+            <label for="reg-password"></label>
+            <input type="password" id="reg-password" placeholder="Enter your password" required>
+            <button type="button" onclick="register()">Register</button>
+        </form>
+    `;
+    if (loginForm.parentNode) {
+        loginForm.parentNode.replaceChild(registerForm, loginForm);
+    } else {
+        loginForm.style.display = 'none';
+        document.getElementById('wrapper').appendChild(registerForm);
+    }
+}
+// Import the necessary Firebase authentication provider
+var googleProvider = new firebase.auth.GoogleAuthProvider();
+function signInWithGoogle() {
+    firebase.auth().signInWithPopup(googleProvider)
+        .then(function(result) {
+            // Redirect to a specific URL after successful login
+            window.location.href = "https://google.com"; // Replace with your desired URL
+        })
+        .catch(function(error) {
+            alert("Error: " + error.message);
+        });
+}
